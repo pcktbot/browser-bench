d66a6c480d5fd01d5127566c6945b818704832f8
David Miller <15317284+pcktbot@users.noreply.github.com>
2023-08-29 09:41:56 -0500

    getting a little brittle

--- puppeteer.js
+++ puppeteer.js
@@ -1,12 +1,4 @@
 const puppeteer = require('puppeteer');
-// const fs = require('fs');
-// const path = require('path');
-
-/**
- * input#Username
- * button[type="submit"]
- * 
- */
 
 class Crawler {
   constructor (params) {
@@ -29,7 +21,7 @@ class Crawler {
 
   async run (creds) {
     for (const cred of creds) {
-      console.log('running credentials', cred);
+      console.info(`Testing ${cred.description}...`);
       await this.openBrowser(this.config);
       await this.openPage();
       await this.navigateTo(cred.entrypoint);
@@ -43,62 +35,47 @@ class Crawler {
   }
 
   async openBrowser (config = this.config) {
-    console.log('opening browser');
     this.browser = await puppeteer.launch(config);
   }
 
   async openPage () {
-    console.log('opening page');
     this.page = await this.browser.newPage();
   }
 
  async navigateTo (entrypoint) {
-    console.log('navigating to entrypoint', entrypoint);
-    await this.page.goto(entrypoint);
+    await this.page.goto(entrypoint, { timeout: 0 });
   }
 
   async waitForNavigation () {
-    console.log('waiting for navigation');
     await this.page.waitForNavigation();
   }
 
   async sortResult (cred) {
-    console.log('sorting result');
     const url = this.page.url();
-    console.log('ended at url', url);
     cred.result_url = url;
     if (cred.should_have_access && cred.entrypoint === url) {
-      console.log('succeeded, should have access', cred);
       this.succeeded = cred;
     } else if (!cred.should_have_access && cred.entrypoint !== url) {
-      console.log('succeeded, should not have access', cred);
       this.succeeded = cred;
     } else {
-      console.error('failed', cred);
       this.failed = cred;
     }
   }
   
   async closePage () {
-    console.log('closing page');
     await this.page.close();
   }
 
   async closeBrowser () {
-    console.log('closing browser');
     await this.browser.close();
   }
 
   async login (cred) {
     // should we store the CSS selector for the login markup in a separate config?
-    this.page.waitForSelector("input#Username");
     await this.page.locator("input#Username").fill(cred.username);
     this.page.click("button[type='submit']");
     await this.waitForNavigation();
-    console.log(this.page.url());
-    this.page.waitForSelector("input#Password");
     await this.page.locator("input#Password").fill(cred.password);
-    console.log('clicking submit');
     this.page.click("button[type='submit']");
     await this.waitForNavigation();
   }
